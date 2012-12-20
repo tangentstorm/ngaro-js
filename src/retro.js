81226e4b6441542b4bfb825c97abecf7bb4eddad
tangentstorm <michal.wallace@gmail.com>
2012-12-20 01:07:38 -0600

    retro.js : cleaned up display of the stacks to pass ngarotests.py

--- src/retro.js
+++ src/retro.js
@@ -1,34 +1,37 @@
-// ngaro module
-var ngaro = require('./ngaro.js');
-// node modules
-var fs = require('fs');
-var path = require('path');
+var
+  fs = require('fs'),
+  path = require('path'),
+  ngaro = require('./ngaro.js');
 
-// start retro.js
-if (require.main === module) {
-    run();
+
+/**
+ * join a typed array with spaces
+ */
+function join(a) {
+  var chars = [ ];
+  for(var i = 0; i < a.length; i++) {
+    chars.push(a[i]);
+  }
+  return chars.join(' ');
 }
 
-function dump(){
-
-    var dumpArray = convertArray(ngaro.ram.image);
-    console.log(String.fromCharCode(28));
-    console.log(dumpArray.join(" "));
-    console.log(String.fromCharCode(29));
-    dumpArray = convertArray(ngaro.address.data);
-    console.log(dumpArray.join(" "));
-    console.log(String.fromCharCode(29));
-    dumpArray = convertArray(ngaro.data.data);
-    console.log(dumpArray.join(" "));
+function chr(n) {
+  return String.fromCharCode(n);
 }
 
-function convertArray(typedArray){
 
-    var newArray = [];
-    for(var i = 0; i < typedArray.length; i++){
-        newArray.push(typedArray[i]);
-    }
-    return newArray;
+/**
+ * dump the virtual machine state, for ngarotest.py
+ */
+function dump() {
+  console.log([
+    chr(28),
+    join(ngaro.data.data.slice(1, ngaro.data.sp + 1 )),
+    chr(29),
+    join(ngaro.address.data.slice(1, ngaro.address.sp + 1 )),
+    chr(29),
+    join(ngaro.ram.image)
+  ].join(''));
 }
 
 function run(){
@@ -38,6 +41,7 @@ function run(){
     var dump_after = false;
     var num = 2;
 
+    // parse args
     while (num < process.argv.length){
         var args = process.argv[num];
         if (args === '--with') {
@@ -58,14 +62,19 @@ function run(){
         num++;
     }
 
+    // default image
     if(!imageFile) {
         imageFile = path.resolve(__dirname,'retro-11.5.img');
     }
 
+
+    // load the specified image
     var fileInfo = fs.statSync(imageFile);
     var cells = Math.ceil(fileInfo.size/4);
-    var imageArray = new Array();
     var fd = fs.openSync(imageFile,'r');
+    var ok = false;
+    var imageArray;
+
 
     if (!fd) {
         console.log('Unable to open file');
@@ -73,17 +82,29 @@ function run(){
         var bufsize = cells*4;
         var buffer = new Buffer(bufsize);
         fs.readSync(fd,buffer,0,bufsize,0);
+
+	imageArray = new Int32Array(cells);
         for (var i = 0; i < cells; i++) {
             imageArray[i] = buffer.readInt32LE(i*4);
         }
         fs.close(fd);
-        ngaro.setImage(imageArray);
-        ngaro.rxProcessImage();
+	ok = true;
     }
 
-    if(!dump_after){
-       //do other stuff
-    } else {
-       dump();
+
+    if ( ok ) {
+        ngaro.setImage(imageArray);
+        ngaro.rxPrepareVM();
+        ngaro.rxProcessImage();
+        if(dump_after) {
+           dump(cells);
+        }
     }
+}
+
+
+
+// start retro.js
+if (require.main === module) {
+  run();
 }
